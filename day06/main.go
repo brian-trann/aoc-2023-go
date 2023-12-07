db59eb35a3f99800408d05337fb012e4ca7274f2
Brian Tran <70680670+brian-trann@users.noreply.github.com>
2023-12-06 18:59:47 -0800

    refactor day6

--- day06/main.go
+++ day06/main.go
@@ -8,8 +8,29 @@ import (
 )
 
 func main() {
-	// partOne()
-	partTwo()
+	contents := utils.OpenFileAsString("./input.txt")
+	// fmt.Println(contents)
+	lines := strings.Split(contents, "\n")
+	var timeTable [][]int
+	var timeTablePartTwo [][]int
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		startIndex := strings.Index(line, ":")
+		timeRowStr := strings.TrimSpace(line[startIndex+1:])
+		timeRowStr2 := strings.ReplaceAll(timeRowStr, " ", "")
+		timeRow := utils.ConvertStringToIntSlice(timeRowStr, " ")
+		timeRow2 := utils.ConvertStringToIntSlice(timeRowStr2, " ")
+		timeTable = append(timeTable, timeRow)
+		timeTablePartTwo = append(timeTablePartTwo, timeRow2)
+	}
+	partOneAnswer := processTimeTable(timeTable)
+	partTwoAnswer := processTimeTable(timeTablePartTwo)
+
+	fmt.Println("p1:", partOneAnswer)
+	fmt.Println("p2:", partTwoAnswer)
 
 }
 func isValid(input int, time int, distance int) bool {
@@ -17,30 +38,15 @@ func isValid(input int, time int, distance int) bool {
 	distanceTraveled := tempDist * input
 	return distanceTraveled > distance
 }
-func partOne() {
-	contents := utils.OpenFileAsString("./input.txt")
-	// fmt.Println(contents)
-	lines := strings.Split(contents, "\n")
-	var timeTable [][]int
+func processTimeTable(table [][]int) int {
 
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
-		startIndex := strings.Index(line, ":")
-		timeRowStr := strings.TrimSpace(line[startIndex+1:])
-		timeRow := utils.ConvertStringToIntSlice(timeRowStr, " ")
-		timeTable = append(timeTable, timeRow)
-	}
-	// fmt.Println(timeTable)
 	var allValues []int
-	for i := range timeTable[0] {
+	for i := range table[0] {
 
-		// fmt.Println(timeTable[0][i])
 		// this block is each race
 		raceNumber := i + 1
-		time := timeTable[0][i]
-		distance := timeTable[1][i]
+		time := table[0][i]
+		distance := table[1][i]
 		fmt.Printf("Race: %d | time: %d | distance: %d\n\n", raceNumber, time, distance)
 		validOpts := 0
 		for j := 1; j < time; j++ {
@@ -54,45 +60,5 @@ func partOne() {
 		}
 	}
 	product := utils.ProductOfArray(allValues)
-	fmt.Println("part one answer", product)
-}
-func partTwo() {
-	contents := utils.OpenFileAsString("./input.txt")
-	// fmt.Println(contents)
-	lines := strings.Split(contents, "\n")
-	var timeTable [][]int
-
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
-		startIndex := strings.Index(line, ":")
-		timeRowStr := strings.TrimSpace(line[startIndex+1:])
-		newTimeRow := strings.ReplaceAll(timeRowStr, " ", "")
-		timeRow := utils.ConvertStringToIntSlice(newTimeRow, " ")
-		timeTable = append(timeTable, timeRow)
-	}
-	// fmt.Println(timeTable)
-	var allValues []int
-	for i := range timeTable[0] {
-
-		// fmt.Println(timeTable[0][i])
-		// this block is each race
-		raceNumber := i + 1
-		time := timeTable[0][i]
-		distance := timeTable[1][i]
-		fmt.Printf("Race: %d | time: %d | distance: %d\n\n", raceNumber, time, distance)
-		validOpts := 0
-		for j := 1; j < time; j++ {
-			if isValid(j, time, distance) {
-				validOpts++
-			}
-		}
-		fmt.Println("ValidOpts: ", validOpts)
-		if validOpts > 0 {
-			allValues = append(allValues, validOpts)
-		}
-	}
-	product := utils.ProductOfArray(allValues)
-	fmt.Println("part two answer", product)
+	return product
 }
